6fe8f55bc6cce75a38e244f113c1204ed465180a
sufix1976 <exog.aj@gmail.com>
2026-04-27 07:31:33 +0200

    Update App.TEST.v4.tsx

--- src/App.TEST.v4.tsx
+++ src/App.TEST.v4.tsx
@@ -1061,6 +1061,9 @@ if (!distMiddle.length) {
         const shortExitProjected = projectMarkerPointsToCandles(sim.shortExitPoints, candles, "above-near");
         const blockedLongProjected = projectMarkerPointsToCandles(real.blockedLongPoints, candles, "below-mid");
         const blockedShortProjected = projectMarkerPointsToCandles(real.blockedShortPoints, candles, "above-mid");
+        const realBuyProjected = projectMarkerPointsToCandles(real.realBuyPoints, candles, "below-near");
+        const realSellProjected = projectMarkerPointsToCandles(real.realSellPoints, candles, "above-near");
+        const realCloseProjected = projectMarkerPointsToCandles(real.realClosePoints, candles, "inside-mid");
         
 
         candidateLongSeries.setData(candidateLongProjected as any);
