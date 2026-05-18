b74eb9d572ef7d55f5d9464a24dc8615bb973498
sufix1976 <exog.aj@gmail.com>
2026-05-18 13:15:55 +0200

    Update App.TEST.v4.tsx

--- src/App.TEST.v4.tsx
+++ src/App.TEST.v4.tsx
@@ -1,6 +1,6 @@
 import { Fragment, useEffect, useMemo, useRef, useState } from "react";
 // @ts-ignore
-import { computeQTrendCore } from "./qtrend-core";
+import { computeQTrendCore, buildTradeReplay } from "./qtrend-core";
 import {
   createChart,
   createSeriesMarkers,
