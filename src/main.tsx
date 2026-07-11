6f875d850cfb4a0bf5a62e8d40b402a4ecbb1151
sufix1976 <exog.aj@gmail.com>
2026-07-11 10:18:05 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.test.v5.tsx'
+import App from './App.test.v6.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
